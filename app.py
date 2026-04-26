8665db64db34b8120af9fd5ff26b56e90d7c2334
hissah055 <452010119@stu.ut.edu.sa>
2026-04-26 15:26:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def get_logo_html():
         logo_base64 = base64.b64encode(logo_path.read_bytes()).decode()
         return (
             f'<img src="data:image/png;base64,{logo_base64}" '
-            f'style="width:260px; height:110px; object-fit:contain; margin-right:40px;">'
+            f'style="width:340px; height:190px; object-fit:contain; margin-right:45px;">'
         )
 
     return '<div style="font-size:60px; margin-right:20px;">📊</div>'
